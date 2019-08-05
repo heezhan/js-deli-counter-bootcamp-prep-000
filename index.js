391d77e01f1e990757fab454ba084c24cb325025
Heejae Han <hhan1194@gmail.com>
2019-08-05 10:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function takeANumber (katzDeliLine, name) {
+/* function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+} */
+var totalnumber = 0
+function takeANumber (katzDeliLine) {
+  totalnumber++
+  katzDeliLine.push(totalnumber)
+  return `Welcome, you are number ${totalnumber}`
 }
 
 function nowServing (katzDeliLine) {
@@ -18,9 +24,14 @@ function currentLine (katzDeliLine) {
   }
   var i = 0
   while (i < katzDeliLine.length) {
+
     katzDeliLine[i] = ` ${i+1}. ${katzDeliLine[i]}`
     i++
   }
   return `The line is currently:` + katzDeliLine
-}
+
+    katzDeliLine[i] = i + katzDeliLine[i]
+    i++
+  }
+
   
